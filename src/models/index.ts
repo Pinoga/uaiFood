da31313d83bd1ce4624c3d5ab71b77c55591cfd5
fbizzotto <fbizzotto@adverge.com.br>
2021-01-10 23:23:42 -0300

    Ajustes na documentacao e delecao de arquivos antigos

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,6 +3,9 @@ import { Model, model } from 'mongoose';
 import ItemSchema, { IItem } from './item';
 import RestaurantSchema, { IRestaurant } from './restaurant';
 
+<<<<<<< HEAD
 //Exportação dos modelos do Mongoose
+=======
+>>>>>>> d422cba40e86277677cca1f6291e415cd6526a3a
 export const Item = model<IItem>('item', ItemSchema);
 export const Restaurant = model<IRestaurant>('restaurant', RestaurantSchema);
--- src/models/restaurant/index.ts
+++ src/models/restaurant/index.ts
@@ -3,6 +3,22 @@ import { Document, Schema } from 'mongoose';
 //Schema do modelo do Restaurante no MongoDB
 //Apenas os IDs dos itens são salvos nos documentos, não os objetos
 const RestaurantSchema = new Schema({
+
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 10        
+    },
+
     name: {
         type: String,
         required: true,
@@ -26,6 +42,19 @@ const RestaurantSchema = new Schema({
     },
     items: [{type: Schema.Types.ObjectId, ref: 'item'}],
 });
+RestaurantSchema.index({location: '2dsphere'});
+RestaurantSchema.index({cuisineType: 'text'});
+
+export interface IRestaurant extends Document {
+    email: string,
+    password: string,
+    name: string,
+    cuisineType: string,
+    city: string,
+    location: [Number],
+    items: [Schema.Types.ObjectId]
+}
+
 
 //O tipo de cozinha é indexado para a busca por similaridade
 RestaurantSchema.index({cuisineType: 'text'});
